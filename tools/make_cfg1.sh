e242f2522f46a7462256a77395f8e597767ac146
Roc Vallès <vallesroc@gmail.com>
2010-10-31 22:43:34 +0000

    Fixed cat abuse.

--- tools/make_cfg1.sh
+++ tools/make_cfg1.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-cat cfg1hdr.bin >cfg1.bin
-cat stage1.bin >>cfg1.bin
+cat cfg1hdr.bin stage1.bin >cfg1.bin
 dd if=/dev/zero bs=1 count=$((3840-`stat -L -c %s stage1.bin`)) >>cfg1.bin
 tools/bin2header/bin2header cfg1.bin cfg1.h port1_config_descriptor
 
